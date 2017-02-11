88d71cea39a8b08303f30382222b42592ba2c233
ScottHo <sscott.ho@gmail.com>
2017-02-11 13:44:02 -0700

    Added the actual files for controlers

---
+++ cam/Controller.cpp
@@ -0,0 +1,30 @@
+#include "Controller.h"
+#include <iostream>
+
+
+#include <stdexcept>
+#include "windows.h"
+#include "Xinput.h"
+
+
+
+
+Controller::Controller() {
+	XINPUT_STATE state;
+	ZeroMemory(&state, sizeof(XINPUT_STATE));
+
+	if (XInputGetState(0, &state) == ERROR_SUCCESS){
+		std::cout << "Controller is connected";
+	}
+	else
+	{
+		std::cout << "Controller is not connected";
+	}
+}
+
+
+Controller::~Controller() {
+}
+
+void Controller::Update() {
+}
---
+++ cam/Controller.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "windows.h"
+#include "Xinput.h"
+
+class Controller {
+public:
+	Controller();
+	~Controller();
+
+	void Update();
+private:
+};
