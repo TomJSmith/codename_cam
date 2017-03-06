3d7af3e32447c6bf3c5f5a6bb11173c8d15a9ecd
ScottHo <sscott.ho@gmail.com>
2017-03-06 04:27:39 -0700

    Added spring to camera

--- cam/Camera.cpp
+++ cam/Camera.cpp
@@ -24,7 +24,7 @@ void Camera::RegisterHandlers()
 	entity_->GetEvents().RegisterEventHandler(&handler_);
 
 	auto p = entity_->GetTransform().position;
-	PxVec3 offset(0, 5, -20);
+	PxVec3 offset(0, 8, -20);
 
 	auto gp = entity_->GetGlobalPosition();
 	auto gr = entity_->GetGlobalRotation();
@@ -34,7 +34,7 @@ void Camera::RegisterHandlers()
 
 		std::shared_ptr<RigidBody> body(new RigidBody(
 			phys,
-			*phys.GetPhysics()->createMaterial(0.0f, 0.0f, 1.0f),
+			*phys.GetPhysics()->createMaterial(0.0f, 0.0f, 0.0f),
 			PxSphereGeometry(3.0f),
 			PxTransform(PxVec3(gp.x + offset.x, gp.y + offset.y, gp.z + offset.z))
 		));
@@ -43,6 +43,7 @@ void Camera::RegisterHandlers()
 
 		auto rb = body->GetRigidBody();
 		rb->is<PxRigidDynamic>()->setMass(0.001f);
+
 		auto tactor = t->GetActor();
 
 		auto d6 = PxD6JointCreate(
@@ -52,6 +53,20 @@ void Camera::RegisterHandlers()
 			rb,
 			PxTransform(-offset)
 		);
+
+		// THESE SETTING PRETTY MUCH DONT MAKE SENSE.
+		d6->setConstraintFlag(PxConstraintFlag::eCOLLISION_ENABLED, false);
+		d6->setConstraintFlag(PxConstraintFlag::ePROJECTION, true);
+		d6->setProjectionAngularTolerance(5.0f);
+		//d6->setMotion(PxD6Axis::eX, PxD6Motion::eLIMITED);
+		d6->setMotion(PxD6Axis::eZ, PxD6Motion::eLIMITED);
+		d6->setMotion(PxD6Axis::eSWING1, PxD6Motion::eLIMITED);
+		//d6->setMotion(PxD6Axis::eSWING2, PxD6Motion::eLIMITED);
+		d6->setMotion(PxD6Axis::eTWIST, PxD6Motion::eLIMITED);
+		//d6->setDrive(PxD6Drive::eSLERP, PxD6JointDrive(10000000.0f, 10000.0f, FLT_MAX));
+		d6->setTwistLimit(PxJointAngularLimitPair(.01f, 0.2f, PxSpring(1000.0f, 500.0f)));
+		d6->setSwingLimit(PxJointLimitCone::PxJointLimitCone(.3f, 1.0f, PxSpring(10000000.0f, 10000000.0f)));
+		d6->setLinearLimit(PxJointLinearLimit(3.0f, PxSpring(1000.0f, 500.0f)));
 	}
 }
 
