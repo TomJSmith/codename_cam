1146d2365a4771bc3f531602a39a90641810705e
JohnnySimmonds <simmondsjohnny@gmail.com>
2017-03-25 18:30:23 -0600

    BEst commit ever

--- cam/main.cpp
+++ cam/main.cpp
@@ -71,14 +71,14 @@ int main() {
 			plane->AddComponent(std::move(planemesh));
 			plane->AddComponent(std::move(planebody));
 
-			/*
+			
 			auto aiVehicle = Entity::Create(root.get()).lock();
 			std::shared_ptr<Component> aiMesh(new Mesh(std::unique_ptr<Shader>(new ModelShader("runner_texture.jpg")), "runner_mesh.fbx", vec3(1.0, 0.84, 0.0), vec3(4.427f, 2.426f, 12.935f), GL_TRIANGLES));
 			std::shared_ptr<Component> aiV(new ScriptComponent("chaser_ai", physics));
 			aiVehicle->AddComponent(std::move(aiMesh));
 			aiVehicle->AddComponent(std::move(aiV));
 			aiVehicle->AddComponent(std::make_unique<ScriptComponent>("chaser", physics));
-			*/
+			
 			
 			auto vehicle = Entity::Create(root.get()).lock();
 			std::shared_ptr<Component> mesh(new Mesh(std::unique_ptr<Shader>(new ModelShader("runner_texture.jpg")), "runner_mesh.fbx", vec3(0.1, 0.1, 0.6), vec3(4.427f, 2.426f, 12.935f), GL_TRIANGLES));
